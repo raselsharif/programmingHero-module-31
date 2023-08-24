7aef09e6d35d83376abb3526c14937347f98ea5a
Rasel Sharif <raselsharif9@gmail.com>
2023-08-24 17:23:46 +0600

    added class

---
+++ class.js
@@ -0,0 +1,15 @@
+class Teacher {
+
+    constructor(name, subject) {
+        this.name = name;
+        this.subject = subject;
+    }
+}
+
+const rafique =new Teacher('Rafique', 'Science');
+
+console.log(rafique);
+
+const rajia =new Teacher('Rajia', 'English');
+
+console.log(rajia);
