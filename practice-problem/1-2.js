7fd62c7758fe605d0761ee99bd539b0b8122d028
Rasel Sharif <raselsharif9@gmail.com>
2023-08-26 21:33:34 +0600

    fixed problem 1.2

--- practice-problem/1-2.js
+++ practice-problem/1-2.js
@@ -1,9 +1,13 @@
 
 const arr = [33, 50, 79, 78, 90, 101, 30];
 
+// filter method
+
 const result = arr.filter(num => num % 10 === 0);
 console.log(result);
 
 
+
+// find method
 const result1 = arr.find(num => num % 10 === 0);
 console.log(result1);
